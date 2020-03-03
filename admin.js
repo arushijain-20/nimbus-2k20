edb692c7658fceefee8fc80fda3f307e3530e5d9
RohitKaushal7 <rohitkk252550@gmail.com>
2020-03-03 17:17:27 +0530

    admin bug-fix

--- admin.js
+++ admin.js
@@ -2,15 +2,23 @@ User = require("./userSchema");
 
 module.exports = function(req, res) {
   console.log(req.body);
-
-  User.findOne({ admin: true }).then(admin => {
-    let ppass = JSON.parse(JSON.stringify(admin)).pass;
-    if (req.body.pass == ppass) {
-      User.find().then(users => {
-        res.json(users);
-      });
-    } else {
+  User.findOne({ admin: true })
+    .then(admin => {
+      let ppass = JSON.parse(JSON.stringify(admin)).pass;
+      if (req.body.pass == ppass) {
+        User.find().then(users => {
+          res.json(users);
+          return;
+        });
+      } else {
+        res.json({ msg: "WP" });
+        return;
+      }
+    })
+    .catch(err => {
+      console.log(err);
       res.json({ msg: "WP" });
-    }
-  });
+    });
+
+  return;
 };
